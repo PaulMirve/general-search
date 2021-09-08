bdb7b53b536e7941aa8354ea8086a114c8a36529
Paul Miranda <paul_mv3@hotmail.com>
2021-09-08 09:15:27 -0700

    added folders and css utils

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import {
     Router,
-    Route
+    Route,
+    Switch
 } from "react-router-dom";
+import Landing from './components/layout/Landing';
 import history from './history';
 
 
 export default function App() {
     return (
         <Router history={history}>
-            <h1>Hello World!</h1>
+            <Switch>
+                <Route exact path="/" component={Landing} />
+            </Switch>
         </Router>
     )
 }
